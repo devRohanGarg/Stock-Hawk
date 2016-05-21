9d3deacc18a62ae4b5954e2be06c6b8d67a8be53
Rohan Garg <dev.RohanGarg@gmail.com>
2016-05-22 01:58:55 +0530

    Add required variables to widget ListItem

--- app/src/main/java/com/sam_chordas/android/stockhawk/widget/ListItem.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/ListItem.java
@@ -4,5 +4,5 @@ package com.sam_chordas.android.stockhawk.widget;
  * Created by Rohan Garg on 18-05-2016.
  */
 public class ListItem {
-    public String heading, content;
+    public String symbol, name, bid_price, change, currency, percent_change;
 }
